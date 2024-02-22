c7bd68063a36a0c89a1758291e3f7281352ac832
Oliver Buchmann <o.buchmann@pc-web.at>
2024-02-22 10:44:48 +0100

    initial version

---
+++ nodes/ZammadTicketUpdate/ZammadTicketUpdate.node.ts
@@ -0,0 +1,379 @@
+import type {
+	IExecuteFunctions,
+	ICredentialsDecrypted,
+	ICredentialTestFunctions,
+	IDataObject,
+	ILoadOptionsFunctions,
+	INodeCredentialTestResult,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+} from 'n8n-workflow';
+
+import {
+	ticketUpdateDescription
+} from './descriptions/TicketUpdateDescription';
+
+import {
+	doesNotBelongToZammad,
+	fieldToLoadOption,
+	getAllFields, getAllStates,
+	getGroupCustomFields,
+	getGroupFields,
+	getOrganizationCustomFields,
+	getOrganizationFields,
+	getTicketCustomFields,
+	getTicketFields,
+	getUserCustomFields,
+	getUserFields,
+	isCustomer,
+	isNotZammadFoundation,
+	tolerateTrailingSlash,
+	zammadApiRequest,
+} from './GenericFunctions';
+
+import type {Zammad as ZammadTypes} from './types';
+
+export class ZammadTicketUpdate implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'ZammadTicketUpdate',
+		name: 'zammad_ticket_update',
+		icon: 'file:ZammadTicketUpdate.svg',
+		group: ['input'],
+		version: 1,
+		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+		description: 'Update Tickets using the Zammad API',
+		defaults: {
+			name: 'Zammad Ticket Update',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		credentials: [
+			{
+				name: 'zammadBasicAuthApi',
+				required: true,
+				testedBy: 'zammadBasicAuthApiTest',
+				displayOptions: {
+					show: {
+						authentication: ['basicAuth'],
+					},
+				},
+			},
+			{
+				name: 'zammadTokenAuthApi',
+				required: true,
+				testedBy: 'zammadTokenAuthApiTest',
+				displayOptions: {
+					show: {
+						authentication: ['tokenAuth'],
+					},
+				},
+			},
+		],
+		properties: [
+			{
+				displayName: 'Authentication',
+				name: 'authentication',
+				type: 'options',
+				options: [
+					{
+						name: 'Basic Auth',
+						value: 'basicAuth',
+					},
+					{
+						name: 'Token Auth',
+						value: 'tokenAuth',
+					},
+				],
+				default: 'tokenAuth',
+			},
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				noDataExpression: true,
+				type: 'options',
+				options: [
+					{
+						name: 'Ticket',
+						value: 'ticket',
+					},
+				],
+				default: 'ticket',
+			},
+
+			...ticketUpdateDescription
+		],
+	};
+
+	methods = {
+		loadOptions: {
+			// ----------------------------------
+			//          custom fields
+			// ----------------------------------
+
+			async loadGroupCustomFields(this: ILoadOptionsFunctions) {
+				const allFields = await getAllFields.call(this);
+
+				return getGroupCustomFields(allFields).map(fieldToLoadOption);
+			},
+
+
+			async loadOrganizationCustomFields(this: ILoadOptionsFunctions) {
+				const allFields = await getAllFields.call(this);
+
+				return getOrganizationCustomFields(allFields).map(fieldToLoadOption);
+			},
+
+			async loadUserCustomFields(this: ILoadOptionsFunctions) {
+				const allFields = await getAllFields.call(this);
+
+				return getUserCustomFields(allFields).map(fieldToLoadOption);
+			},
+
+			async loadTicketCustomFields(this: ILoadOptionsFunctions) {
+				const allFields = await getAllFields.call(this);
+
+				return getTicketCustomFields(allFields).map((i) => ({name: i.name, value: i.name}));
+			},
+
+			// ----------------------------------
+			//          built-in fields
+			// ----------------------------------
+
+			async loadGroupFields(this: ILoadOptionsFunctions) {
+				const allFields = await getAllFields.call(this);
+
+				return getGroupFields(allFields).map(fieldToLoadOption);
+			},
+
+			async loadOrganizationFields(this: ILoadOptionsFunctions) {
+				const allFields = await getAllFields.call(this);
+
+				return getOrganizationFields(allFields).map(fieldToLoadOption);
+			},
+
+			async loadTicketFields(this: ILoadOptionsFunctions) {
+				const allFields = await getAllFields.call(this);
+
+				return getTicketFields(allFields).map(fieldToLoadOption);
+			},
+
+			async loadUserFields(this: ILoadOptionsFunctions) {
+				const allFields = await getAllFields.call(this);
+
+				return getUserFields(allFields).map(fieldToLoadOption);
+			},
+
+			async loadTicketStates(this: ILoadOptionsFunctions) {
+				const states = await getAllStates.call(this)
+				return states.map(s => {
+					return {name: s.name, value: s.name}
+				})
+			},
+
+			// ----------------------------------
+			//             resources
+			// ----------------------------------
+
+			// by non-ID attribute
+
+
+			/**
+			 * PUT /users requires organization name instead of organization ID.
+			 */
+			async loadOrganizationNames(this: ILoadOptionsFunctions) {
+				const orgs = (await zammadApiRequest.call(
+					this,
+					'GET',
+					'/organizations',
+				)) as ZammadTypes.Group[];
+
+				return orgs.filter(isNotZammadFoundation).map((i) => ({name: i.name, value: i.name}));
+			},
+
+			/**
+			 * POST & PUT /tickets requires customer email instead of customer ID.
+			 */
+			async loadCustomerEmails(this: ILoadOptionsFunctions) {
+				const users = (await zammadApiRequest.call(this, 'GET', '/users')) as ZammadTypes.User[];
+
+				return users.filter(isCustomer).map((i) => ({name: i.email, value: i.email}));
+			},
+
+			// by ID
+
+			async loadGroups(this: ILoadOptionsFunctions) {
+				const groups = (await zammadApiRequest.call(this, 'GET', '/groups')) as ZammadTypes.Group[];
+
+				return groups.map((i) => ({name: i.name, value: i.id}));
+			},
+
+			async loadOrganizations(this: ILoadOptionsFunctions) {
+				const orgs = (await zammadApiRequest.call(
+					this,
+					'GET',
+					'/organizations',
+				)) as ZammadTypes.Organization[];
+
+				return orgs.filter(isNotZammadFoundation).map((i) => ({name: i.name, value: i.id}));
+			},
+
+			async loadUsers(this: ILoadOptionsFunctions) {
+				const users = (await zammadApiRequest.call(this, 'GET', '/users')) as ZammadTypes.User[];
+
+				return users.filter(doesNotBelongToZammad).map((i) => ({name: i.login, value: i.id}));
+			},
+		},
+		credentialTest: {
+			async zammadBasicAuthApiTest(
+				this: ICredentialTestFunctions,
+				credential: ICredentialsDecrypted,
+			): Promise<INodeCredentialTestResult> {
+				const credentials = credential.data as ZammadTypes.BasicAuthCredentials;
+
+				const baseUrl = tolerateTrailingSlash(credentials.baseUrl);
+
+				const options: any = {
+					method: 'GET',
+					uri: `${baseUrl}/api/v1/users/me`,
+					json: true,
+					rejectUnauthorized: !credentials.allowUnauthorizedCerts,
+					auth: {
+						user: credentials.username,
+						pass: credentials.password,
+					},
+				};
+
+				try {
+					await this.helpers.request(options);
+					return {
+						status: 'OK',
+						message: 'Authentication successful',
+					};
+				} catch (error) {
+					return {
+						status: 'Error',
+						message: error.message,
+					};
+				}
+			},
+
+			async zammadTokenAuthApiTest(
+				this: ICredentialTestFunctions,
+				credential: ICredentialsDecrypted,
+			): Promise<INodeCredentialTestResult> {
+				const credentials = credential.data as ZammadTypes.TokenAuthCredentials;
+
+				const baseUrl = tolerateTrailingSlash(credentials.baseUrl);
+
+				const options: any = {
+					method: 'GET',
+					uri: `${baseUrl}/api/v1/users/me`,
+					json: true,
+					rejectUnauthorized: !credentials.allowUnauthorizedCerts,
+					headers: {
+						Authorization: `Token token=${credentials.accessToken}`,
+					},
+				};
+
+				try {
+					await this.helpers.request(options);
+					return {
+						status: 'OK',
+						message: 'Authentication successful',
+					};
+				} catch (error) {
+					return {
+						status: 'Error',
+						message: error.message,
+					};
+				}
+			},
+		},
+	};
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const items = this.getInputData();
+
+		const resource = this.getNodeParameter('resource', 0) as ZammadTypes.Resource;
+		const operation = this.getNodeParameter('operation', 0);
+
+		let responseData;
+		const returnData: INodeExecutionData[] = [];
+
+		for (let i = 0; i < items.length; i++) {
+			try {
+				if (resource === 'ticket') {
+					// **********************************************************************
+					//                                  ticket
+					// **********************************************************************
+
+					if (operation === 'update') {
+						// ----------------------------------
+						//           ticket:create
+						// ----------------------------------
+
+						// https://docs.zammad.org/en/latest/api/ticket/index.html#create
+
+						const body = {} as any;
+
+						const group = this.getNodeParameter('group', i) as string;
+						const state = this.getNodeParameter('state', i) as string;
+
+						if (group) {
+							body.group = group;
+						}
+						if (state) {
+							body.state = state;
+						}
+
+						const article = this.getNodeParameter('article', i) as ZammadTypes.Article;
+
+						if (article && article.articleDetails) {
+
+							const {
+								articleDetails: {visibility, ...rest},
+							} = article;
+
+							body.article = {
+								...rest,
+								internal: visibility === 'internal',
+							};
+						}
+
+						const { customFieldsUi } = this.getNodeParameter(
+							'additionalFields',
+							i,
+						) as ZammadTypes.UserAdditionalFields;
+
+						customFieldsUi?.customFieldPairs.forEach((pair) => {
+							body[pair.name] = pair.value;
+						});
+
+						const id = this.getNodeParameter('id', i) as string;
+
+						responseData = await zammadApiRequest.call(
+							this,
+							'PUT',
+							`/tickets/${id}`,
+							body
+						);
+					}
+				}
+
+				const executionData = this.helpers.constructExecutionMetaData(
+					this.helpers.returnJsonArray(responseData as IDataObject),
+					{itemData: {item: i}},
+				);
+				returnData.push(...executionData);
+			} catch (error) {
+				if (this.continueOnFail()) {
+					returnData.push({json: {error: error.message}});
+					continue;
+				}
+				throw error;
+			}
+		}
+		return [returnData];
+	}
+}
---
+++ nodes/ZammadTicketUpdate/descriptions/TicketUpdateDescription.ts
@@ -0,0 +1,232 @@
+import type { INodeProperties } from 'n8n-workflow';
+
+export const ticketUpdateDescription: INodeProperties[] = [
+	// ----------------------------------
+	//           operations
+	// ----------------------------------
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		noDataExpression: true,
+		displayOptions: {
+			show: {
+				resource: ['ticket'],
+			},
+		},
+		options: [
+			{
+				name: 'Update',
+				value: 'update',
+				description: 'Update a ticket',
+				action: 'Update a ticket',
+			},
+		],
+		default: 'update',
+	},
+	{
+		displayName: 'Ticket ID',
+		name: 'id',
+		type: 'string',
+		description:
+			'Ticket to update. Specify an ID using an <a href="https://docs.n8n.io/code-examples/expressions/">expression</a>.',
+		default: '',
+		required: false,
+		displayOptions: {
+			show: {
+				resource: ['ticket'],
+				operation: ['update'],
+			},
+		},
+	},
+	{
+		displayName: 'Group Name or ID',
+		name: 'group',
+		type: 'string',
+		placeholder: 'First-Level Helpdesk',
+		description:
+			'Group that will own the ticket. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code-examples/expressions/">expression</a>.',
+		default: '',
+		required: false,
+		displayOptions: {
+			show: {
+				resource: ['ticket'],
+				operation: ['update'],
+			},
+		},
+	},
+	// {
+	// 	displayName: 'Customer Email Name or ID',
+	// 	name: 'customer',
+	// 	type: 'options',
+	// 	typeOptions: {
+	// 		loadOptionsMethod: 'loadCustomerEmails',
+	// 	},
+	// 	description:
+	// 		'Email address of the customer concerned in the ticket to create. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code-examples/expressions/">expression</a>.',
+	// 	default: '',
+	// 	placeholder: 'hello@n8n.io',
+	// 	required: true,
+	// 	displayOptions: {
+	// 		show: {
+	// 			resource: ['ticket'],
+	// 			operation: ['create'],
+	// 		},
+	// 	},
+	// },
+
+	{
+		displayName: 'State',
+		name: 'state',
+		type: 'string',
+		description:
+			'New Ticket State',
+		default: '',
+		required: false,
+		// typeOptions: {
+		// 	loadOptionsMethod: 'loadTicketStates',
+		// },
+		displayOptions: {
+			show: {
+				resource: ['ticket'],
+				operation: ['update'],
+			},
+		},
+	},
+	{
+		displayName: 'Article',
+		name: 'article',
+		type: 'fixedCollection',
+		placeholder: 'Add Article',
+		required: false,
+		default: {},
+		displayOptions: {
+			show: {
+				resource: ['ticket'],
+				operation: ['update'],
+			},
+		},
+		options: [
+			{
+				displayName: 'Article Details',
+				name: 'articleDetails',
+				values: [
+					{
+						displayName: 'Subject',
+						name: 'subject',
+						type: 'string',
+						default: '',
+					},
+					{
+						displayName: 'Body',
+						name: 'body',
+						type: 'string',
+						default: '',
+					},
+					{
+						displayName: 'Visibility',
+						name: 'visibility',
+						type: 'options',
+						default: 'internal',
+						options: [
+							{
+								name: 'External',
+								value: 'external',
+								description: 'Visible to customers',
+							},
+							{
+								name: 'Internal',
+								value: 'internal',
+								description: 'Visible to help desk',
+							},
+						],
+					},
+					{
+						displayName: 'Article Type',
+						name: 'type',
+						type: 'options',
+						// https://docs.zammad.org/en/latest/api/ticket/articles.html
+						options: [
+							{
+								name: 'Chat',
+								value: 'chat',
+							},
+							{
+								name: 'Email',
+								value: 'email',
+							},
+							{
+								name: 'Fax',
+								value: 'fax',
+							},
+							{
+								name: 'Note',
+								value: 'note',
+							},
+							{
+								name: 'Phone',
+								value: 'phone',
+							},
+							{
+								name: 'SMS',
+								value: 'sms',
+							},
+						],
+						default: 'note',
+					},
+				],
+			},
+		],
+	},
+	{
+		displayName: 'Additional Fields',
+		name: 'additionalFields',
+		type: 'collection',
+		displayOptions: {
+			show: {
+				resource: ['ticket'],
+				operation: ['update'],
+			},
+		},
+		default: {},
+		placeholder: 'Add Field',
+		options: [
+			{
+				displayName: 'Custom Fields',
+				name: 'customFieldsUi',
+				type: 'fixedCollection',
+				default: {},
+				placeholder: 'Add Custom Field',
+				typeOptions: {
+					multipleValues: true,
+				},
+				options: [
+					{
+						name: 'customFieldPairs',
+						displayName: 'Custom Field',
+						values: [
+							{
+								displayName: 'Field Name',
+								name: 'name',
+								type: 'options',
+								typeOptions: {
+									loadOptionsMethod: 'loadTicketCustomFields',
+								},
+								default: '',
+								description:
+									'Name of the custom field to set. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code-examples/expressions/">expression</a>.',
+							},
+							{
+								displayName: 'Value',
+								name: 'value',
+								type: 'string',
+								default: '',
+								description: 'Value to set on the custom field',
+							},
+						],
+					},
+				],
+			},
+		],
+	},
+];
